f49c1263df583f07eed3b41322e89bd5675aacde
Cameron Hetzler <cjhetz@gmail.com>
2023-12-06 01:30:45 -0500

    Removing test case

--- src/test/java/com/cjhetz/hellospringboot/HelloSpringBootApplicationTests.java
+++ src/test/java/com/cjhetz/hellospringboot/HelloSpringBootApplicationTests.java
@@ -1,13 +1,8 @@
 package com.cjhetz.hellospringboot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelloSpringBootApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
