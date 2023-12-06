13eb4cffe907da107d3b25df73f3730acae1248f
Cameron Hetzler <cjhetz@gmail.com>
2023-12-06 01:31:26 -0500

    Removing test case

--- src/test/java/com/cjhetz/hellospringboot/HelloSpringBootApplicationTests.java
+++ src/test/java/com/cjhetz/hellospringboot/HelloSpringBootApplicationTests.java
@@ -2,6 +2,7 @@ package com.cjhetz.hellospringboot;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+// https://stackoverflow.com/questions/66600210/illegalstate-failed-to-load-applicationcon
 @SpringBootTest
 class HelloSpringBootApplicationTests {
 
